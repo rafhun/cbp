249b7452c24e289a6537267da241fc52d33a2896
Raphael Hüni <hueni@werbelinie.ch>
2015-02-13 16:02:57 +0100

    test jade and favicons

--- grunt/favicons.js
+++ grunt/favicons.js
@@ -4,6 +4,9 @@ module.exports = {
     coast: true,
     firefox: true,
     androidHomescreen: true,
+    appleTouchBackgroundColor: '#fff', // auto, none or #color
+    tileBlackWhite: true, // make white-only icon on win8 tile
+    tileColor: '#00f', // auto, none or #color -> this is important if above is true
   },
   icons: {
     src: '<%= pkg.srcFolders.images %>favicon.png',
