4e3c05e65e2951c7ce6bba9e332929564de45e28
Raphael Hüni <hueni@werbelinie.ch>
2015-02-17 10:33:58 +0100

    remove contrexx from framework
    installation or rather downloading and copying the files has to be done
    manually to keep all relevant files and ensure that the latest version
    is used. as soon as comvation releases a permanent link for the latest
    version a fix will be made to automatically download the cms through
    a grunt task.

--- wwwroot/lib/javascript/jquery/ui/i18n/jquery.ui.datepicker-default.js
+++
@@ -1,6 +0,0 @@
-﻿/* Default initialisation for the jQuery UI date picker plugin. */
-/* By Douglas Jose & Juerg Meier. */
-jQuery(function($){
-	$.datepicker.setDefaults({dateFormat: 'dd.mm.yy'});
-	$.timepicker.setDefaults({timeFormat: 'hh:mm:ss'});
-});
--- wwwroot/modules/shop/template/frontend/_readme.txt
+++
@@ -1,2 +0,0 @@
-This folder contains default templates.
-Use those to restore proper functionality if nothing else works.
