3cb4a4797f6885c718ceb543d5d876c49282c5bf
Raphael Hüni <hueni@werbelinie.ch>
2015-03-13 15:35:29 +0100

    remove fonts.com tracking script
    is no longer needed since we are loading the font
    from their servers

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -2,13 +2,3 @@
 // to this file. Unlike the script.js file this one does not get linted
 // by grunt as code quality should be given by the developers of the plugin
 // Therefore be careful as to what you include to not weaken your site
-
-// fonts.com tracking script
-var MTIProjectId='5d4b0b0b-3738-4d30-a729-255608ae8b41';
- (function() {
-        var mtiTracking = document.createElement('script');
-        mtiTracking.type='text/javascript';
-        mtiTracking.async='true';
-        mtiTracking.src=('https:'==document.location.protocol?'https:':'http:')+'//fast.fonts.net/t/trackingCode.js';
-        (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild( mtiTracking );
-   })();
