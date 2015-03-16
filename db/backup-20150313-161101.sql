d5789dbfdcd046cd5d7fafbbe220e9411f7c2d51
Raphael Hüni <rafhungmail.com>
2015-03-16 06:36:07 +0100

    remove warning line

--- db/backup-20150313-161101.sql
+++ db/backup-20150313-161101.sql
@@ -1,4 +1,3 @@
-Warning: Using a password on the command line interface can be insecure.
 -- MySQL dump 10.13  Distrib 5.6.23, for osx10.10 (x86_64)
 --
 -- Host: localhost    Database: christen_ortho
