dd4ed8a1f41e8e48df9157379db84e34a20af911
Raphael Hüni <hueni@werbelinie.ch>
2015-03-16 16:28:52 +0100

    add watch task for customizings

--- grunt/watch.js
+++ grunt/watch.js
@@ -1,68 +1,46 @@
 module.exports = {
   options: {
     livereload: true,
+    spawn: false
   },
   jsHint: {
     files: ['<%= pkg.srcFolders.js %>main.js', 'grunt/*.js', 'Gruntfile.js'],
-    tasks: ['jshint'],
-    options: {
-      spawn: false,
-    }
+    tasks: ['jshint']
   },
   scripts: {
     files: ['<%= pkg.srcFolders.js %>**/*.js'],
-    tasks: ['clean:hashedJs', 'concat', 'uglify', 'cssmin', 'jade', 'hashres'],
-    options: {
-      spawn: false,
-    }
+    tasks: ['clean:hashedJs', 'concat', 'uglify', 'cssmin', 'jade', 'hashres']
   },
   styles: {
     files: ['<%= pkg.srcFolders.scss %>**/*.scss'],
-    tasks: ['clean:hashedCss', 'sass', 'autoprefixer', 'cssmin', 'uglify', 'jade', 'hashres', 'hologram'],
-    options: {
-      spawn: false,
-    }
+    tasks: ['clean:hashedCss', 'sass', 'autoprefixer', 'cssmin', 'uglify', 'jade', 'hashres', 'hologram']
   },
   images: {
     files: ['<%= pkg.srcFolders.images %>**/*.{png,jpg,gif}'],
-    tasks: ['imagemin'],
-    options: {
-      spawn: false,
-    }
+    tasks: ['imagemin']
   },
   svg: {
     files: ['<%= pkg.srcFolders.svg %>*.svg'],
-    tasks: ['svgstore', 'jade', 'hashres'],
-    options: {
-      spawn: false,
-    }
+    tasks: ['svgstore', 'jade', 'hashres']
   },
   grunticon: {
     files: ['<%= pkg.srcFolders.icons %>*.svg'],
-    tasks: ['grunticon'],
-    options: {
-      spawn: false,
-    }
+    tasks: ['grunticon']
   },
   favicons: {
     files: ['<%= pkg.srcFolders.images %>favicon.png'],
-    tasks: ['clean:hashes','favicons', 'htmlmin', 'jade', 'hashres'],
-    options: {
-      spawn: false,
-    }
+    tasks: ['clean:hashes','favicons', 'htmlmin', 'jade', 'hashres']
   },
   jade: {
     files: ['<%= pkg.srcFolders.jade %>*.{jade,html}'],
-    tasks: ['clean:hashes', 'jade', 'cssmin', 'uglify','hashres'],
-    options: {
-      spawn: false,
-    }
+    tasks: ['clean:hashes', 'jade', 'cssmin', 'uglify','hashres']
+  },
+  customizing: {
+    files: ['<%= pkg.srcFolders.customizing %>**'],
+    tasks: ['clean:customizing', 'copy:customizing']
   },
   hologram: {
     files: ['<%= pkg.srcFolders.scss %>README.md'],
-    tasks: ['hologram'],
-    options: {
-      spawn: false,
-    }
+    tasks: ['hologram']
   }
 }
