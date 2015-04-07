36462ab24aee0e256da8f422aa75608e8ec33036
Raphael Hüni <hueni@werbelinie.ch>
2015-04-07 14:50:34 +0200

    Fix off canvas navigation
    JS now targets anchor elements themselves not the li's which
    fixes the issues experienced with second level navigation.

--- src/js/components/off-canvas.js
+++ src/js/components/off-canvas.js
@@ -2,29 +2,43 @@
 jQuery(document).ready(function($) {
   var transformer = $('.transformer'),
     menuToggle = $('.off-canvas-toggle'),
-    linkItem = $('.off-canvas-list-item'),
-    upLink = $('.off-canvas-up');
-    // subMenu = $('.menu');
+    linkItem = $('.js-oc-link'),
+    content = $('.content-container'),
+    subMenu = $('.menu');
 
   menuToggle.on('click', function(e) {
     e.preventDefault();
+    
+    $('html,body').animate({
+      scrollTop: 0
+    }, 1000);
+
     transformer.toggleClass('in');
   }); // end menuToggle.on click
 
+  
+  content.click(function() {
+    if (transformer.hasClass('in')) {
+      transformer.removeClass('in');
+    }
+  }); // end content.click
+
+  // add the back link to all submenus since contrexx is to stupid to do it automatically
+  subMenu.prepend('<li class="off-canvas-list-item"><a href="#" class="js-oc-up off-canvas-link icon-back">Zurück</a></li>');
+  subMenu.on('click', '.js-oc-up', function(e) {
+    var $this = $(this);
+    e.preventDefault();
+    // alert('test');
+    $this.parent().parent().removeClass('in');
+  }); // end upLink.on click
+
   linkItem.on('click', function(e) {
     var $this = $(this);
-    if ($this.find('.menu').length) {
+    if ($this.siblings('.menu').length) {
       e.preventDefault();
-      $this.find('.menu').toggleClass('in');
+      $this.siblings('.menu').toggleClass('in');
     } else {
       return true;
     }
   }); // end linkItem.on click
-
-  upLink.on('click', function(e) {
-    var $this = $(this);
-    e.preventDefault();
-
-    $this.parent('.menu.in').removeClass('in');
-  }); // end upLink.on click
 }); // end ready
