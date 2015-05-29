641e420d33ae48159c0c066def3e48274f3861bd
Raphael Hüni <hueni@werbelinie.ch>
2015-05-29 15:28:27 +0200

    Add the rsync task
    Will enable rsync deployment over SSH. Seems to work properly however cannot
    be fully tested at this point in time since we need the password first.
    
    Also is there a possibility to set up a key exchange so no password is needed
    after initial connect? Or is there a way to pass the password from the
    `secret.json` file?

---
+++ grunt/rsync.js
@@ -0,0 +1,23 @@
+module.exports = {
+  options: {
+    args: ['--verbose'],
+    recursive: true,
+  },
+  production: {
+    options: {
+      src: 'wwwroot/',
+      dest: '<%= secrets.production.ascms_root %>',
+      host: '<%= secrets.production.sshHost',
+      delete: true
+    }
+  },
+  productionDry: {
+    options: {
+      args: ['--verbose', '--dry-run'],
+      src: 'wwwroot/',
+      dest: '<%= secrets.production.ascms_root %>',
+      host: '<%= secrets.production.sshHost %>',
+      delete: true
+    }
+  }
+}
