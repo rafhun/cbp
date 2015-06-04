6a77396c4d497da51fbec016abbc276640001a35
Raphael Hüni <rafhungmail.com>
2015-06-04 07:22:59 +0200

    clean up grunt

--- grunt/clean.js
+++ grunt/clean.js
@@ -5,7 +5,6 @@ module.exports = {
   html: ['<%= destFolder %>*.html'],
   images: ['<%= destFolder %><%= pkg.buildFolders.img %>'],
   dest: ['<%= destFolder %>'],
-  unhashed: ['<%= destFolder %>{style.min.css,script.min.js}'],
   contrexx: ['wwwroot/images/**/*.{jpg,jpeg,gif,png,thumb,pdf,ico,db,html}', 'wwwroot/themes/skeleton_3_0'],
   editorStyles: ['<%= destFolder %>style-ck.css'],
   editorConfig: ['wwwroot/core/Wysiwyg/ckeditor.config.js.php'],
