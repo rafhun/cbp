df2926908918c7ad658530b12405e4c685ee9a05
Raphael Hüni <rafhungmail.com>
2015-06-04 07:31:05 +0200

    fix bump task

--- grunt/bump.js
+++ grunt/bump.js
@@ -10,7 +10,7 @@ module.exports = {
       tagName: 'v%VERSION%',
       tagMessage: 'Version %VERSION%',
       push: true,
-      pushTo: 'upstream',
+      pushTo: 'origin',
       gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
       globalReplace: false,
       prereleaseName: false,
