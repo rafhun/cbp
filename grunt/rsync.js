498089d55b1a501bd714a602ef5922cd9bcb17d2
Raphael Hüni <hueni@werbelinie.ch>
2015-06-16 10:15:43 +0200

    feat(build): add rsync push tasks
    We can now automatically build our theme and push it to the live
    or staging environment through rsync, given the necessary config.
    Some good default options have been set and tasks set up to ease
    the process.
    
    Careful though: rsync will only work if the ssh keys have been set
    up properly.

--- grunt/rsync.js
+++ grunt/rsync.js
@@ -2,12 +2,22 @@ module.exports = {
   options: {
     args: ['--verbose'],
     recursive: true,
+    exclude: ['.git', '.hg', '.svn', '.DS_Store', '._*', 'Thumbs.db', '/tmp/cache/**'],
+    include: ['/tmp/cache/.htaccess'],
+  },
+  staging: {
+    options: {
+      src: 'wwwroot/',
+      dest: '<%= secrets.staging.ascms_root %>',
+      host: '<%= secrets.staging.sshHost %>',
+      delete: true
+    }
   },
   production: {
     options: {
       src: 'wwwroot/',
       dest: '<%= secrets.production.ascms_root %>',
-      host: '<%= secrets.production.sshHost',
+      host: '<%= secrets.production.sshHost %>',
       delete: true
     }
   },
