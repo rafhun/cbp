9bbe8d9ebb95c09b2265e184fbbae5fce54783b6
Raphael Hüni <hueni@werbelinie.ch>
2015-07-27 15:51:52 +0200

    feat(cms): add .htaccess template
    We add the template and the tasks to automatically copy it
    to the root folder on every grunt run. We might want to
    consider temporarily disabling this or setting it through
    a flag to make sure we do not have to deal with too many
    caching issues. On the other hand it is great to work in the
    same exact environment the end user will experience.

--- grunt/clean.js
+++ grunt/clean.js
@@ -1,14 +1,15 @@
 module.exports = {
+  contrexx: ['wwwroot/images/**/*.{jpg,jpeg,gif,png,thumb,pdf,ico,db,html}', 'wwwroot/themes/skeleton_3_0'],
+  customizing: ['<%= config.buildFolders.customizing %>'],
+  dest: ['<%= destFolder %>'],
+  editorConfig: ['<%= config.buildFolders.editorConfig %>ckeditor.config.js.php'],
+  editorStyles: ['<%= destFolder %>style-ck.css'],
+  favicon: ['<%= config.srcFolders.jade %>favicons.html'],
   hashes: ['<%= destFolder %>{style.min.*.css,script.min.*.js}'],
   hashedCss: ['<%= destFolder %>style.min.*.css'],
   hashedJs: ['<%= destFolder %>script.min.*.js'],
+  htaccess: ['<%= config.buildFolders.root %>.htaccess'],
   html: ['<%= destFolder %>*.html'],
   images: ['<%= destFolder %><%= config.buildFolders.img %>'],
-  dest: ['<%= destFolder %>'],
-  contrexx: ['wwwroot/images/**/*.{jpg,jpeg,gif,png,thumb,pdf,ico,db,html}', 'wwwroot/themes/skeleton_3_0'],
-  editorStyles: ['<%= destFolder %>style-ck.css'],
-  editorConfig: ['<%= config.buildFolders.editorConfig %>ckeditor.config.js.php'],
-  customizing: ['<%= config.buildFolders.customizing %>'],
-  minSvg: ['<%= config.srcFolders.iconsMin %>**/*.svg'],
-  favicon: ['<%= config.srcFolders.jade %>favicons.html']
+  minSvg: ['<%= config.srcFolders.iconsMin %>**/*.svg']
 }
--- grunt/copy.js
+++ grunt/copy.js
@@ -1,4 +1,32 @@
 module.exports = {
+  customizing: {
+    files: [{
+      expand: true,
+      cwd: '<%= config.srcFolders.customizing %>',
+      src: ['**'],
+      dest: '<%= config.buildFolders.customizing %>'
+    }]
+  },
+  editorConfig: {
+    files: [{
+      src: '<%= config.srcFolders.config %>ckeditor.config.js.php',
+      dest: '<%= config.buildFolders.editorConfig %>ckeditor.config.js.php'
+    }]
+  },
+  editorCustomizing: {
+    files: [{
+      expand: true,
+      cwd: '<%= config.srcFolders.customizing %>lib/ckeditor/',
+      src: '**/*.js',
+      dest: '<%= config.buildFolders.root%>lib/ckeditor/'
+    }]
+  },
+  editorStyles: {
+    files: [{
+      src: '<%= config.srcFolders.css %>editor-styles.css',
+      dest: '<%= destFolder %>editor-styles.css'
+    }]
+  },
   fonts: {
     files: [
       {
@@ -10,32 +38,10 @@ module.exports = {
       }
     ]
   },
-  editorConfig: {
+  htaccess: {
     files: [{
-      src: '<%= config.srcFolders.config %>ckeditor.config.js.php',
-      dest: '<%= config.buildFolders.editorConfig %>ckeditor.config.js.php'
-    }]
-  },
-  editorStyles: {
-    files: [{
-      src: '<%= config.srcFolders.css %>editor-styles.css',
-      dest: '<%= destFolder %>editor-styles.css'
-    }]
-  },
-  customizing: {
-    files: [{
-      expand: true,
-      cwd: '<%= config.srcFolders.customizing %>',
-      src: ['**'],
-      dest: '<%= config.buildFolders.customizing %>'
-    }]
-  },
-  editorCustomizing: {
-    files: [{
-      expand: true,
-      cwd: '<%= config.srcFolders.customizing %>lib/ckeditor/',
-      src: '**/*.js',
-      dest: '<%= config.buildFolders.root%>lib/ckeditor/'
+      src: '<%= config.srcFolders.config %>.htaccess',
+      dest: '<%= config.buildFolders.root %>.htaccess'
     }]
   }
 }
